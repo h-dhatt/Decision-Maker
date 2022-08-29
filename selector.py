086b9ea538a35eb606e93172d527f9ddee5c25d4
h-dhatt <111666380+h-dhatt@users.noreply.github.com>
2022-08-29 02:11:49 -0400

    Update selector.py

--- selector.py
+++ selector.py
@@ -1,21 +1,21 @@
 import random
 
-friends = [
-    'Olek',
-    'Carmela',
-    'Laura',
-    'Megan',
-    'Keith',
-    'Anna',
-    'Skyler',
-    'Amy',
-    'Nadya'
+activities = [
+    'Read a book',
+    'Play an instrument',
+    'Call a friend',
+    'Try origami',
+    'Watch a movie',
+    'Build a puzzle',
+    'Write a short story',
+    'Draw a cat',
+    'Clean your room'
 ]
 
 # random.randint(1, 5) --> random number between 1 and 5
 # random.choice(array) --> random item in this array
 
-selected = random.choice(friends) # randomly choose a friend
+selected = random.choice(activities) # randomly choose a friend
 
-print('Who should I facetime today?')
+print('What should I do today?')
 print(selected)
